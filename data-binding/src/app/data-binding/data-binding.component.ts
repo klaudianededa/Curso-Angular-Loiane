932b3f760718ce968098acaeda33563c54ab032d
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-15 19:04:51 -0300

    two-way data binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -24,7 +24,12 @@ export class DataBindingComponent implements OnInit {
 
   isMouseOver: boolean = false;
 
-  nome: string = '';
+  nome: string = 'abc';
+
+  pessoa: any = {
+    nome: 'def',
+    idade: 20
+  }
 
   getValor() {
     return 1;
