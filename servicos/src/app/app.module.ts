c5ad5a6514656e8ca575d7ebb61c1a3c74c380a0
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-23 22:01:03 -0300

    escopo de instancias de serviços e modulos

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { CursosModule } from './cursos/cursos.module';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
