87df111a953a936e32d146dcfcdbd9b646a0ed87
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-26 16:56:44 -0300

    Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
   styleUrls: ['./aluno-detalhe.component.css']
 })
-export class AlunoDetalheComponent implements OnInit {
+export class AlunoDetalheComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  aluno: any;
+  inscricao: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private alunosService: AlunosService
+  ) { }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+      }
+    );
   }
 
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
 }
--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AlunosService } from './../alunos.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-aluno-form',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoFormComponent implements OnInit {
 
-  constructor() { }
+  aluno: any = {};
+  inscricao: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private alunosService: AlunosService
+  ) { }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+
+        if (this.aluno === null) {
+          this.aluno = {};
+        }
+      }
+    );
   }
 
+  editarContato() {
+    this.router.navigate(['/alunos', this.aluno.id, 'editar']);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
 }
