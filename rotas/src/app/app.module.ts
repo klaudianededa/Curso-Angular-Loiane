6f0d2c6ce8b24bd02e9f5317aefae13d09597d74
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-26 13:49:40 -0300

    Criando um módulo de rotas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 
-import { routing } from './app.routing';
+//import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    AppRoutingModule
+    //routing
   ],
 
   providers: [CursosService],
