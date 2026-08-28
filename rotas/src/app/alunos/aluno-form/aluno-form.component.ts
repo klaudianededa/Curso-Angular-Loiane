188c3f0f5c5f20d66d5854b0044fb6755ae137e5
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-28 11:06:04 -0300

    Guarda de Rotas: CanDeactivate com Interface Genérica

--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -2,13 +2,14 @@ import { AlunosService } from './../alunos.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IFormCanDeactivate } from 'src/app/guards/iform-candeactivete';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit {
+export class AlunoFormComponent implements OnInit, IFormCanDeactivate {
 
   aluno: any = {};
   inscricao: Subscription;
@@ -51,5 +52,9 @@ export class AlunoFormComponent implements OnInit {
       confirm('Tem certeza que deseja sair dessa página?');
     }
     return true;
+  }
+
+  podeDesativar() {
+    return this.podeMutarRota();
   }
 }
--- rotas/src/app/guards/alunos.deactivate.guard.ts
+++ rotas/src/app/guards/alunos.deactivate.guard.ts
@@ -2,19 +2,21 @@ import { Observable } from 'rxjs/Rx';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AlunoFormComponent } from './../alunos/aluno-form/aluno-form.component';
+import { IFormCanDeactivate } from './iform-candeactivete';
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> {
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
 
   canDeactivate(
-    component: AlunoFormComponent,
+    component: IFormCanDeactivate,
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
 
     console.log('guarda de desativação');
 
-     return component.podeMutarRota ? component.podeMudarRota() : true;
-    //return Observable.of(false);
+    //return component.podeMutarRota ? component.podeMudarRota() : true;
+
+    return component.podeDesativar ? component.podeDesativar() : true;
   }
 }
