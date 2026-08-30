caf69071d66aeec06624c76adbbaa156e51fe6ea
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-30 15:06:28 -0300

    Formulários reativos: Configuração (Módulo e Componente)

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 import { HttpClientModule } from '@angular/common/http';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     TemplateFormModule,
-    HttpClientModule
+    HttpClientModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ forms/src/app/data-form/data-form.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataFormComponent } from './data-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    DataFormComponent
+  ]
+})
+export class DataFormModule { }
