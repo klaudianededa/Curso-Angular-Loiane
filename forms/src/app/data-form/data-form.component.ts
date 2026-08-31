a78e25a48023130391ff715962fcbacb0d20707b
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-31 14:44:27 -0300

    Campos de endereço (migrando um form template driven para form reativo)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -11,58 +11,73 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private http: HttpClient
   ) { }
 
   ngOnInit(): void {
-    /*this.formulario = new FormGroup({
-      nome: new FormControl(null),
-      email: new FormControl(null),
-    });*/
 
     this.formulario = this.formBuilder.group({
-      nome: [null, Validators.required, Validators.minLength(3), Validators.maxLength(20)],
-      email: [null, [Validators.required, Validators.email]]
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: [null, [Validators.required, Validators.email]],
+      cep: [null, Validators.required],
+      numero: [null, Validators.required],
+      complemento: [null],
+      rua: [null, Validators.required],
+      bairro: [null, Validators.required],
+      cidade: [null, Validators.required],
+      estado: [null, Validators.required]
     });
+
   }
 
   onSubmit() {
+
     console.log(this.formulario.value);
 
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+    this.http.post(
+      'https://httpbin.org/post',
+      JSON.stringify(this.formulario.value)
+    )
       .subscribe(
         dados => {
           console.log(dados);
+
           // reseta o form
           // this.formulario.reset();
-          //this.resetar();
+          // this.resetar();
         },
         (error: any) => alert('erro')
       );
   }
+
   resetar() {
     this.formulario.reset();
   }
 
-  verificaValidTouched(campo: any) {
-
-    this.formulario.get(campo).valid &&  this.formulario.get(campo)?.touched
-    return !campo.valid && campo.touched;
+  verificaValidTouched(campo: string) {
+    return !this.formulario.get(campo)?.valid &&
+           this.formulario.get(campo)?.touched;
   }
 
-verificaEmailInvalido(){
-  let campoEmail = this.formulario.get('email');
-if(campoEmail.errors){
-  return campoEmail.errors['email'] && campoEmail?.touched;
-}
-}
+  verificaEmailInvalido() {
 
-  aplicaCssErro(campo: any) {
+    let campoEmail = this.formulario.get('email');
+
+    if (campoEmail?.errors) {
+      return campoEmail.errors['email'] && campoEmail.touched;
+    }
+
+  }
+
+  aplicaCssErro(campo: string) {
+
     return {
       'class.has-error': this.verificaValidTouched(campo),
       'class.has-feedback': this.verificaValidTouched(campo)
     };
+
   }
 
 }
