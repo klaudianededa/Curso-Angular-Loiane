0b026c2fadd63f7f46dfed2ed68d657729b84115
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-01 15:02:07 -0300

    Serviço de Mensagens de Erros

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
 import { DropdownService } from './services/dropdown.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ErrorMsgComponent } from './error-msg/error-msg.component';
 
 
 
@@ -14,12 +15,14 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   declarations: [
     FormDebugComponent,
-    CampoControlErroComponent
+    CampoControlErroComponent,
+    ErrorMsgComponent
   ],
   exports: [
     FormDebugComponent,
     CampoControlErroComponent,
-    HttpClientModule
+    HttpClientModule,
+    ErrorMsgComponent
   ],
   providers: [ DropdownService ]
 })
