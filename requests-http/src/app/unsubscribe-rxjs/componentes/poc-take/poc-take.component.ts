f8bd23f554a7c2a996a306dc3228563087145c4a
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-07 18:50:45 -0300

    poc base

---
+++ requests-http/src/app/unsubscribe-rxjs/componentes/poc-take/poc-take.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poc-take',
+  templateUrl: './poc-take.component.html',
+  styleUrls: ['./poc-take.component.scss']
+})
+export class PocTakeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
