57ca2f246f3ad4fdb44b38ec373a71b8a03e1bc0
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-07 19:08:10 -0300

    RxJS: Unsubscribe Automático

--- requests-http/src/app/unsubscribe-rxjs/componentes/poc-take-until/poc-take-until.component.ts
+++ requests-http/src/app/unsubscribe-rxjs/componentes/poc-take-until/poc-take-until.component.ts
@@ -10,14 +10,26 @@ import { tap, take } from 'rxjs/operators';
     </app-poc-base>
   `
 })
-export class PocTakeComponent implements OnInit {
+export class PocTakeUntilComponent implements OnInit, OnDestroy {
 
   nome = 'Componente com take';
   valor: string;
 
+  unsub$ = new Subject();
+
   constructor(private service: EnviarValorService) { }
 
   ngOnInit() {
-
+    this.service.getValor()
+      .pipe(
+        tap(v => console.log(this.nome, v)),
+        takeUntil(this.unsub$)
+      )
+      .subscribe(novoValor => this.valor = novoValor);
+  }
+  ngOnDestroy() {
+    this.unsub$.next();
+    this.unsub$.complete();
+    console.log(`${this.nome} foi destruido`);
   }
 }
--- requests-http/src/app/unsubscribe-rxjs/componentes/poc-take/poc-take.component.ts
+++ requests-http/src/app/unsubscribe-rxjs/componentes/poc-take/poc-take.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-poc-take',
   templateUrl: './poc-take.component.html',
   styleUrls: ['./poc-take.component.scss']
 })
-export class PocTakeComponent implements OnInit {
+export class PocTakeComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    this.service.getValor()
+      .pipe(
+        tap(v => console.log(this.nome, v)),
+        take(1)
+      )
+      .subscribe(novoValor => this.valor = novoValor);
+  }
+  ngOnDestroy() {
+    console.log(`${this.nome} foi destruido`);
   }
 
 }
