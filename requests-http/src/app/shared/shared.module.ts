e3035c08d60ad15935b868bc4e6a0f05283f2d16
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-08 19:57:02 -0300

    Popup de Confirmação genérica Bootstrap 4 (com RxJS)

--- requests-http/src/app/shared/shared.module.ts
+++ requests-http/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertModalComponent } from './alert-modal/alert-modal.component';
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AlertModalComponent],
+  declarations: [AlertModalComponent, ConfirmModalComponent],
   exports: [AlertModalComponent],
   entryComponents: [AlertModalComponent]
 })
