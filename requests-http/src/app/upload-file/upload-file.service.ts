2083093c253f39394b83f327f296bee5bebc10a8
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-14 14:17:17 -0300

    Barra de Progresso + Observando Eventos Http

--- requests-http/src/app/upload-file/upload-file.service.ts
+++ requests-http/src/app/upload-file/upload-file.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UploadFileService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    upload(files: Set<File>, url: string) {
+  upload(files: Set<File>, url: string) {
 
-        const formData = new FormData();
-        files.forEach(file => formData.append('file', file, file.name));
+    const formData = new FormData();
+    files.forEach(file => formData.append('file', file, file.name));
 
-        // const request = new HttpRequest('POST', url, formData);
-        // return this.http.request(request);
+    // const request = new HttpRequest('POST', url, formData);
+    // return this.http.request(request);
 
-        return this.http.post(url, formData);
-    }
+    return this.http.post(url, formData, {
+      observe: 'events',
+      reportProgress: true
+    });
+  }
 }
--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
 import { take } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
+<button type="button" class="btn btn-primary"(click) = "onUpload()"[disabled] = "!files" >
+  Upload
+  </button>
 
 @Component({
   selector: 'app-upload-file',
@@ -11,6 +14,7 @@ import { environment } from '../../../environments/environment';
 export class UploadFileComponent implements OnInit {
 
   files: Set<File>;
+  progress = 0;
 
   constructor(private service: UploadFileService) { }
 
@@ -29,12 +33,24 @@ export class UploadFileComponent implements OnInit {
       this.files.add(selectedFiles[i]);
     }
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, environment.BASE_URL + '/upload')
-        .subscribe(response => console.log('Upload Concluído'));
+        .subscribe((event: HttpEvent<Object>) => {
+          // HttpEventType
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log('Upload Concluído');
+          } else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total);
+            console.log('Progresso', percentDone);
+            this.progress = percentDone;
+          }
+        });
     }
   }
 }
